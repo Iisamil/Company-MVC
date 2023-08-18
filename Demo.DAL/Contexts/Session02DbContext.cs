413e17aa9357774e8504e0ef273c4adb4fd1e1ce
Iisamil <101970115+Iisamil@users.noreply.github.com>
2023-08-18 17:59:25 +0300

    Add files via upload

---
+++ Demo.DAL/Contexts/Session02DbContext.cs
@@ -0,0 +1,30 @@
+﻿using Demo.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Contexts
+{
+    public class Session02DbContext : IdentityDbContext<ApplicationUser>
+    {
+        public Session02DbContext(DbContextOptions<Session02DbContext> options) : base(options)
+        {
+            // ay 7d 3awz yklm el db hyklm el ctor da w lazm yb3t haga mn DbContexOptions
+        }
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //    => optionsBuilder.UseSqlServer("Server = . ; Database = SessionTwoMVC ; Trusted_Connection = true ; MultipleActiveResultSets = true");
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+
+        //public DbSet<IdentityUser> Users { get; set; }
+
+        //public DbSet<IdentityRole> Roles { get; set; }
+
+
+    }
+}
---
+++ Demo.DAL/Models/Department.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.DAL.Models
+{
+    public class Department
+    {
+        public int Id { get; set; } // PK by Convension
+
+        [Required(ErrorMessage = "Code is Required !")]
+        public string Code { get; set; } // nvarchar(max) not required in .net 5
+
+        [Required (ErrorMessage = "Name is Required !")]
+        [MaxLength(50)]
+        public string Name { get; set; }  
+        public DateTime DateOfCreation { get; set; }
+
+
+        // Navigational Property => MANY
+        public ICollection<Employee> Employees { get; set; } = new HashSet<Employee>();
+
+    }
+}
